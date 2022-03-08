530f49c3285ae8ae47308c35de5bab9c22361776
chotnho <builinhduong43@gmail.com>
2022-03-08 15:25:12 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+//Ẩn hiện giỏ hàng
 document.getElementById("showcart").style.display="none";
 
-
+// thêm đơn hang
 var giohang = new Array();
 
 function themvaogiohang(x) {
@@ -15,9 +16,12 @@ function themvaogiohang(x) {
 
     console.log(giohang);
     showcountsp();
+// lưu giỏ hàng dùng sessionStorage và Json.stringify (chuyển hàm thành chuỗi)
 
+    sessionStorage.setItem("giohang", JSON.stringify(giohang));
 }
 
+//tạo đơn hàng trong giỏ hàng
 function showcountsp() {
     document.getElementById("countsp").innerHTML = giohang.length;
 }
@@ -48,7 +52,7 @@ function showmycart(){
     document.getElementById("mycart").innerHTML=ttgh;
 
 }
-
+// ẩn hiện  giỏ hàng
 function showcart(){
    var  x= document.getElementById("showcart");
    if (x.style.display == "block") {
@@ -56,5 +60,35 @@ function showcart(){
     } else{
         x.style.display ="block";
     }
+
     showmycart();
+
+    }
+function showgiohang_trangthanhtoan(){
+    var gh = sessionStorage.getItem("giohang");
+    var giohang = JSON.parse(gh);
+    var ttgh= "";
+    var tong = 0;
+    for (let i=0; i<giohang.length; i++){
+        var tt = parseInt(giohang[i][1])*parseInt(giohang[i][3]);
+        tong += tt;
+        ttgh+= '<tr>' +
+            ' <td>'+(i+1)+'</td>'+
+            ' <td><img src="'+giohang[i][0]+'" alt=""></td>' +
+            ' <td>'+giohang[i][2]+'</td>' +
+            '  <td>'+giohang[i][1]+'</td>' +
+            '  <td>'+giohang[i][3]+'</td>' +
+            '  <td>' +
+            '  <div>'+tt+'</div>' +
+            '  </td>' +
+            '  </tr>' ;
+
+    }
+    ttgh+= '<tr>' +
+        '  <th colSpan="5">Tổng đơn hàng</th>' +
+        '  <th>' +
+        '  <div>'+tong+'</div>' +
+        '  </th>' +
+        '   </tr>';
+    document.getElementById("mycart").innerHTML=ttgh;
 }
